c2485629cc546bb1f9db3bfb07d44aadab9c78cf
Kshitij Mahale <kshitijmahale02@gmail.com>
2025-05-19 06:32:35 +0530

    deleted unwanted files

--- src/main/java/com/kshitij/placement_helper/repository/InternshipSubmissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kshitij.placement_helper.repository;
-
-import com.kshitij.placement_helper.model.InternshipSubmission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InternshipSubmissionRepository extends JpaRepository<InternshipSubmission, Long> {
-} 
